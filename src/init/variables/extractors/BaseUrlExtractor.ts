433b86f47e67695f224530782490436fb98ed9f9
diego40g <diegopaznaula@hotmail.com>
2022-04-13 12:06:21 -0500

    cambio https por http

--- src/init/variables/extractors/BaseUrlExtractor.ts
+++ src/init/variables/extractors/BaseUrlExtractor.ts
@@ -19,6 +19,6 @@ export class BaseUrlExtractor extends SettingsExtractor {
       return ensureTrailingSlash(args.baseUrl);
     }
     const port = args.port ?? this.defaultPort;
-    return `http://www.dipaz-solid.online:${port}/`;
+    return `https://www.dipaz-solid.online:${port}/`;
   }
 }
